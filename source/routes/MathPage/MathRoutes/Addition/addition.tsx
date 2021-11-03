0069dfb19b015469af3399c00e0b3f2b6e3eaf23
Nurimanshin Lev <lvoonax@gmail.com>
2021-11-03 21:47:13 +0500

    addition, subtraction, multiplication, division

---
+++ source/routes/MathPage/MathRoutes/Addition/addition.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react';
+
+export const AdditionPage: FunctionComponent = () => {
+    return <div>ADDITION</div>;
+};
---
+++ source/routes/MathPage/MathRoutes/Subtraction/SubtractionPage.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from 'react';
+
+export const SubtractionPage: FunctionComponent = () => {
+    return <div>SubtractionPage</div>;
+};
