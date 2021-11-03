ff03655d699cc169f17f15c721e8305143c656ff
gimis-nurimanshin <gimis_nurimanshin@mail.ru>
2021-11-03 15:07:03 +0500

    feat/icons

--- source/components/Sidebar/Sidebar.tsx
+++ source/components/Sidebar/Sidebar.tsx
@@ -9,6 +9,7 @@ import {
 import React, { FunctionComponent } from 'react';
 import classnames from 'classnames/bind';
 import { Link } from 'react-router-dom';
+import { Icon } from 'ui-kit';
 import styles from './Sidebar.module.scss';
 
 const cn = classnames.bind(styles);
@@ -17,12 +18,28 @@ const CLASS_NAME = 'Sidebar';
 export const Sidebar: FunctionComponent = () => {
     return (
         <div className={cn(CLASS_NAME)}>
-            <Link to={HOME_PAGE_ROUTE}>Главная</Link>
-            <Link to={MATH_PAGE_ROUTE}>Математека</Link>
-            <Link to={FIGURES_PAGE_ROUTE}>Фигуры</Link>
-            <Link to={NUMBERS_PAGE_ROUTE}>Цифры</Link>
-            <Link to={ALPHABET_PAGE_ROUTE}>Алфавит</Link>
-            <Link to={COLORS_PAGE_ROUTE}>Цвета</Link>
+            <Link to={HOME_PAGE_ROUTE}>
+                <Icon iconName="home" />
+            </Link>
+            <Link to={MATH_PAGE_ROUTE}>
+                <Icon iconName="math" />
+            </Link>
+            <Link to={FIGURES_PAGE_ROUTE}>
+                {' '}
+                <Icon iconName="figurs" />
+            </Link>
+            <Link to={NUMBERS_PAGE_ROUTE}>
+                {' '}
+                <Icon iconName="number" />
+            </Link>
+            <Link to={ALPHABET_PAGE_ROUTE}>
+                {' '}
+                <Icon iconName="letters" />
+            </Link>
+            <Link to={COLORS_PAGE_ROUTE}>
+                {' '}
+                <Icon iconName="colors" />
+            </Link>
         </div>
     );
 };
--- source/ui-kit/Icon/_constants.ts
+++ source/ui-kit/Icon/_constants.ts
@@ -1,5 +1,21 @@
-import SCHOOL_ICON from 'assets/icons/school_icon.svg';
+import SOUND_ON_ICON from 'assets/icons/sound_on.svg';
+import SOUND_OFF_ICON from 'assets/icons/sound_off.svg';
+import LETTERS_ICON from 'assets/icons/letters.svg';
+import NUMBERS_ICON from 'assets/icons/numbers.svg';
+import FIGURS_ICON from 'assets/icons/figurs.svg';
+import MATH_ICON from 'assets/icons/math.svg';
+import SCHOOL_ICON from 'assets/icons/school.svg';
+import COLORS_ICON from 'assets/icons/colors.svg';
+import HOME_ICON from 'assets/icons/home.svg';
 
 export const ICONS = {
+    sound_on: SOUND_ON_ICON,
+    sound_off: SOUND_OFF_ICON,
+    letters: LETTERS_ICON,
+    number: NUMBERS_ICON,
+    figurs: FIGURS_ICON,
+    math: MATH_ICON,
     school: SCHOOL_ICON,
+    colors: COLORS_ICON,
+    home: HOME_ICON,
 };
